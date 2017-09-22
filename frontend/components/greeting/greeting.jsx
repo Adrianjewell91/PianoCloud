40309ae5daa91762b961f1c3f3394507211b5bb9
Adrian <adrianjewell91@gmail.com>
2017-09-22 13:28:53 -0700

    refactor greeting redirect

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -7,24 +7,22 @@ class Greeting extends React.Component {
   }
 
   //Set up ternary logic to redirect to user profile if logged in.
+  componentDidMount () {
+    if (this.props.currentUser) {
+      this.props.history.push(`/users/${this.props.currentUser.username}`);}
+  }
+
 
   render () {
 
     let search_input = (<input type="text"
                                placeholder="(For Display Only)"></input>)
 
-    let display;
 
-    if (this.props.currentUser) {
-      display = (
-        <Redirect to={`/users/${this.props.currentUser.username}`}></Redirect>
-      )
-    } else {
-      display = (        <div>
-                         <h1>Discover Music with PianoCloud</h1>
-                         <p>{search_input}</p>
-                         </div>);
-    }
+    let display = (        <div>
+                       <h1>Discover Music with PianoCloud</h1>
+                       <p>{search_input}</p>
+                       </div>);
 
     return (
       <div>
