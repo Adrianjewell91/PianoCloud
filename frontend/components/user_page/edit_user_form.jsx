43c57ee4e07a1b556bb130717f9071d5cb6256bc
Adrian <adrianjewell91@gmail.com>
2017-09-24 11:22:15 -0700

    edit form skeleton

---
+++ frontend/components/user_page/edit_user_form.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-route-dom";
+
+class EditUserForm extends React.component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  update(field) {
+
+  }
+
+  handleSubmit(e) {
+    
+  }
+
+  render () {
+    return (
+      <div className="edit-form">
+        <form>
+
+        </form>
+      </div>
+    );
+  }
+}
