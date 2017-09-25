f430bf8170ab91e372cad7deb8ec9f9ce302ed72
Adrian <adrianjewell91@gmail.com>
2017-09-25 09:47:23 -0700

    basic audio player working

--- frontend/components/audio_player/audio_player.jsx
+++ frontend/components/audio_player/audio_player.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 class AudioPlayer extends React.Component {
   constructor(props) {
@@ -8,7 +9,9 @@ class AudioPlayer extends React.Component {
   render() {
     return(
       <div className='footer'>
-        Playback;
+        <ReactAudioPlayer
+          src = "https://s3-us-west-2.amazonaws.com/pianocloud-adrianjewell/tracks/track_recordings/000/000/001/original/02-Impromptu-in-G-flat-major-D.-899.mp3"
+          autoplay controls/>
       </div>
     );
   }
