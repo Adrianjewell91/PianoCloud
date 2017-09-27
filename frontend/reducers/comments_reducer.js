68b9f215725bece3b0a1a71c9c3ff6e832ffb9d5
Adrian <adrianjewell91@gmail.com>
2017-09-26 21:06:19 -0700

    fixed comment bug

--- frontend/reducers/comments_reducer.js
+++ frontend/reducers/comments_reducer.js
@@ -1,6 +1,8 @@
 import {RECEIVE_COMMENT, REMOVE_COMMENT,
         RECEIVE_COMMENTS} from "../actions/comment_actions";
 
+import {ADD_TRACK_TO_PLAY} from "../actions/audio_player_actions";
+
 const CommentsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
@@ -14,6 +16,8 @@ const CommentsReducer = (state = {}, action) => {
           let newState = Object.assign({}, state);
           delete newState[`${action.id}`];
           return newState;
+    case ADD_TRACK_TO_PLAY:
+      return state;
     default:
           return {};
   }
