82f0ca4e7a9e6cae8d498d81a943d6b1b1219c0a
Adrian <adrianjewell91@gmail.com>
2017-09-28 16:13:01 -0700

    config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
 
+let plugins  = [];
+let devPlugins = [];
 
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -13,6 +31,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
