54a97b1e4d4a3a8ba7e19bc70ecf749324b9b15a
Adrian <adrianjewell91@gmail.com>
2017-11-02 17:43:25 -0700

    add first test

---
+++ frontend/actions/__tests__/pianocloud_actions_test.js
@@ -0,0 +1,5 @@
+describe('async actions', () => {
+  test('requestUserTracks gets all tracks by User Id', () => {
+    expect(1).toEqual(2);
+  });
+});
--- frontend/reducers/now_playing_reducer.js
+++ frontend/reducers/now_playing_reducer.js
@@ -20,9 +20,3 @@ const NowPlayingReducer = (state = [], action) => {
 }
 
 export default NowPlayingReducer;
-
-//how exactly do I want to do this? When I press play I want to play the song.
-//When A song finished, it should pop it off and play the next one.
-//When there is a list of songs, and i press play, I should still play the song
-//I just clicked, and what should happen to the other songs, how about push them
-//back a bit.
